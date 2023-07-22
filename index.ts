b97a3581b7ff5b142b3a47e985b58da060ad2828
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-07-21 22:40:23 -0300

    adding typescript

---
+++ index.ts
@@ -0,0 +1,32 @@
+import Config from "./types/Config";
+
+/**
+ * @param {any[]} data Data will become the csv
+ * @param {Config} config this config let yo personalize how to generate te csv (filename, includeHeaders, separator, etc..)
+ */
+export default function generateCSV(config: Config, data: any[]) {
+  let textToWrite = "";
+  const headers = Object.keys(data[0]);
+
+  if (config.includeTitle) {
+    textToWrite = `${config.title}`;
+    for (let i = 0; i < config.qtLineBreak; i++) {
+      textToWrite += "\n";
+    }
+  }
+  if (config.includeHeaders) {
+    textToWrite += headers.join(config.separator) + "\n";
+  }
+  const rows = data.map((element) => {
+    let values = [];
+    for (let key of headers) {
+      values.push(element[key]);
+    }
+    return values.join(config.separator);
+  });
+  textToWrite += rows.join("\n");
+  const resultFileName = config.filename
+    ? `${config.filename}.csv`
+    : "data.csv";
+  return { filename: resultFileName, content: textToWrite };
+}
---
+++ types/Config.ts
@@ -0,0 +1,8 @@
+export default interface Config {
+  filename: string;
+  includeHeaders: boolean;
+  includeTitle: boolean;
+  title: string;
+  separator: string;
+  qtLineBreak: number;
+}
